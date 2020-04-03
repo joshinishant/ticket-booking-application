168793c68c8bcee13f3a464f3546c4217dce4520
I354348 <nishant.joshi01@sap.com>
2020-04-04 01:21:52 +0530

    added sample unit test

--- src/test/java/com/sap/mdm/prd/TicketBookingApplicationTests.java
+++ src/test/java/com/sap/mdm/prd/TicketBookingApplicationTests.java
@@ -1,18 +1,18 @@
-/*
 package com.sap.mdm.prd;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class TicketBookingApplicationTests {
 
 	@Test
-	public void contextLoads() {
-	}
+	public void sampleTest(){
+	    Assert.assertTrue(true);
+    }
 
 }
-*/
